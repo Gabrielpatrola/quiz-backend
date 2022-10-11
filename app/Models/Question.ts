798c6cc61ced37fe6f3da6aeaa179b85ed132e98
Gabriel Augusto Almeida <gabriel@patrola.tech>
2022-10-11 14:55:00 -0400

    feat: first commit

---
+++ app/Models/Question.ts
@@ -0,0 +1,42 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export enum QuestionDifficultyTypes {
+  EASY = 'easy',
+  MEDIUM = 'MEDIUM',
+  HARD = 'HARD',
+}
+
+export enum QuestionTypes {
+  BOOLEAN = 'boolean',
+  MULTIPLE = 'multiple',
+}
+
+export default class Question extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public type: QuestionTypes
+
+  @column()
+  public question: string
+
+  @column()
+  public category: string
+
+  @column()
+  public correct_answer: string
+
+  @column()
+  public incorrect_answers: Array<string>
+
+  @column()
+  public difficulty: QuestionDifficultyTypes
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
---
+++ database/migrations/1665512317224_questions.ts
@@ -0,0 +1,27 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'questions'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('category')
+      table.string('question')
+      table.string('correct_answer')
+      table.enu('difficulty', ['easy', 'medium', 'hard'])
+      table.json('incorrect_answers')
+      table.enu('type', ['multiple', 'boolean'])
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
